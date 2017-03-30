7447ba8d409397ff8cfded0a6a583786f646699d
athul166 <athul.satheesh166@gmail.com>
2017-03-30 19:30:41 +0530

    monitoring completed

--- src/monitor/JobsList.js
+++ src/monitor/JobsList.js
@@ -3,70 +3,119 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 import { Container, Row, Col } from 'react-grid-system';
 import Divider from 'material-ui/Divider';
+import io from 'socket.io-client';
 
 class JobsList extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobList : [],
+      results : ''
+    };
+  }
+  componentDidMount(){
+    this.context.socket.on('result', this.handleData.bind(this));
+  }
+  handleData(results){
+    //var results = JSON.parse(data);
+    //console.log(results.jobId);
+    this.setState({
+      results:results
+    });
+    //console.log(this.state.results);
+    var jobs = this.state.jobList;
+    if(jobs.length!=0) {
+      var flag=0;
+      jobs.map((job) => {
+        if(job.jobId==results.jobId){
+          flag=1;
+        }
+      });
+      if(flag==0){
+        jobs.push({
+          jobId : results.jobId ,
+          jobStatus : this.state.results.stages.status
+        })
+      }
+    } else {
+      jobs.push({
+        jobId : results.jobId ,
+        jobStatus : this.state.results.stages.status
+      })
+    }
+    this.setState({
+      jobList : jobs
+    })
+  }
+
+    render() {
+    var jobs = this.state.jobList.map((job)=>{
+      return(
+        <Container>
+          <Card style={{'width':'100%','margin':'auto','marginTop':10}} onClick={this.props.handleClick.bind(this,1,job.jobId,'')}>
+            <Row>
+                <Col sm={6}>
+                  <CardHeader
+                    title={job.jobId}
+                  />
+                </Col>
+                <Col sm={4} style={{'marginTop':15}}>
+                  started
+                </Col>
+                <Col sm={2} style={{'marginTop':15}}>
+                  <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
+                      <path d="M0-.25h24v24H0z" fill="none"/>
+                  </svg>
+                </Col>
+            </Row>
+        </Card>
+      </Container>
+      );
+    });
     return(
         <div>
-              <Container>
-                <Card style={{'width':'100%','margin':'auto','marginTop':10}} onClick={this.props.handleClick.bind(this,1)}>
-                  <Row>
-                      <Col sm={6}>
-                        <CardHeader
-                          title="CI-Worklow_1"
-                        />
-                      </Col>
-                      <Col sm={4} style={{'marginTop':15}}>
-                       Finished
-                      </Col>
-                      <Col sm={2} style={{'marginTop':15}}>
-                        <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
-                            <path d="M0-.25h24v24H0z" fill="none"/>
-                        </svg>
-                      </Col>
-                  </Row>
-                </Card>
-                <Card style={{'width':'100%','margin':'auto','marginTop':10}}>
-                  <Row>
-                      <Col sm={6}>
-                        <CardHeader
-                          title="CI-Worklow_2"
-                        />
-                      </Col>
-                      <Col sm={4} style={{'marginTop':15}}>
-                        Finished
-                      </Col>
-                      <Col sm={2} style={{'marginTop':15}}>
-                        <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
-                            <path d="M0-.25h24v24H0z" fill="none"/>
-                        </svg>
-                      </Col>
-                  </Row>
-                </Card>
-                <Card style={{'width':'100%','margin':'auto','marginTop':10}}>
-                  <Row>
-                      <Col sm={6}>
-                        <CardHeader
-                          title="CI-Worklow_3"
-                        />
-                      </Col>
-                      <Col sm={4} style={{'marginTop':15}}>
-                        Started
-                      </Col>
-                      <Col sm={2} style={{'marginTop':15}}>
-                        <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
-                            <path d="M0-.25h24v24H0z" fill="none"/>
-                        </svg>
-                      </Col>
-                  </Row>
-                </Card>
-              </Container>
+              {jobs}
         </div>
     );
   }
 }
+JobsList.contextTypes = {
+  socket:React.PropTypes.object
+};
 
 export default JobsList;
+
+
+// componentDidMount(){
+//   this.state.socket.on('result', function(results){
+//   this.setState({
+//       results:results
+//   });
+//   var jobs = this.state.jobList;
+//   console.log(this.state.results);
+//   //console.log("JOBS =====> "+this.state.jobList);
+//   if(jobs.length!=0) {
+//     var flag=0;
+//     jobs.map((job) => {
+//       if(job.jobId==results.jobId){
+//         flag=1;
+//       }
+//     });
+//     if(flag==0){
+//       jobs.push({
+//         jobId : this.state.results.jobId ,
+//         jobStatus : this.state.results.stages.status
+//       })
+//     }
+//   } else {
+//     jobs.push({
+//       jobId : this.state.results.jobId ,
+//       jobStatus : this.state.results.stages.status
+//     })
+//   }
+//   this.setState({
+//     jobList : jobs
+//   })
+// }.bind(this));
+// }
--- src/monitor/Results.js
+++ src/monitor/Results.js
@@ -4,37 +4,71 @@ import Paper from 'material-ui/Paper';
 import { Container, Row, Col } from 'react-grid-system';
 
 class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results : []
+    };
+  }
+  componentDidMount(){
+    this.context.socket.on('result', this.handleData.bind(this));
+  }
+  handleData(results){
+    var Results = this.state.results;
+    if(Results.length==0){
+      Results.push(results);
+    }
+    else{
+      Results.forEach((result,i)=> {
+        if(result.stageName==results.stageName){
+          Results.splice(i,1);
+          // Results.push();
+        } else {
+          Results.push(results);
+        }
+      });
+    }
+    this.setState({
+      results:Results
+    });
+
+  }
   render() {
+    var stageResult=this.state.results.map((result)=>{
+      if(result.stages.status=="Completed" || result.stages.status=="Failed"){
+        if(result.stageName==this.props.stageName){
+          return(
+            <Container>
+              <Tabs style={{'margin':'auto','width':'100%','marginTop':'-15'}}>
+                  <Tab label="STDOUT" style={{'backgroundColor':'#009688'}}>
+                      <div>
+                        {result.stages.result.stdout}
+                      </div>
+                    </Tab>
+                    <Tab label="STDERR" style={{'backgroundColor':'#009688'}}>
+                        {result.stages.result.stderr}
+                   </Tab>
+                   <Tab label="EXITCODE" style={{'backgroundColor':'#009688'}}>
+                   stage exited with code :{result.stages.result.exitCode}
+                   </Tab>
+            </Tabs>
+          </Container>
+          );
+        }
+      }
+    })
     return (
       <div>
         <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" onClick={this.props.handleClick.bind(this,1)}>
             <path d="M0 0h24v24H0z" fill="none"/>
             <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
         </svg>
-        <Container>
-          <Tabs style={{'margin':'auto','width':'100%','marginTop':'-15'}}>
-              <Tab label="STDOUT" style={{'backgroundColor':'#009688'}}>
-                  <div>
-                    <p>1:5  error  'x' is assigned a value but never used  no-unused-vars</p>
-                    <p>2:5  error  'y' is defined but never used           no-unused-vars</p>
-                    <p>:heavy_multiplication_x:  2 problems (2 errors, 0 warnings)</p>
-                  </div>
-                </Tab>
-                <Tab label="STDERR" style={{'backgroundColor':'#009688'}}>
-                    <p>
-                      cannot find module eslint-plugin-node
-                    </p>
-               </Tab>
-               <Tab label="EXITCODE" style={{'backgroundColor':'#009688'}}>
-                  <p>
-                  0
-                  </p>
-               </Tab>
-        </Tabs>
-      </Container>
+        {stageResult}
     </div>
     );
   }
 }
-
+Results.contextTypes = {
+  socket:React.PropTypes.object
+};
 export default Results;
